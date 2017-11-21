bb8c943059a2adb42d74d7eadc737b10f9b8095a
Yurii_Stus <stusyura@gmail.com>
2017-11-21 17:33:33 +0200

    basic usage of jersey and jackson for testing

---
+++ src/main/java/com/rest/utils/Common.java
@@ -0,0 +1,9 @@
+package com.rest.utils;
+
+public class Common {
+
+    public static void log(Object o){
+        System.out.println(o + "");
+    }
+
+}
--- src/main/java/com/rest/utils/JacksonUtils.java
+++ src/main/java/com/rest/utils/JacksonUtils.java
@@ -2,33 +2,60 @@ package com.rest.utils;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
+import java.io.File;
 import java.io.IOException;
+import static com.rest.utils.Common.log;
 
 public class JacksonUtils {
 
-    private static ObjectMapper mapper;
+    private static ObjectMapper mapper = new ObjectMapper();
 
     static {
-        mapper = new ObjectMapper();
         mapper.enable(SerializationConfig.Feature.INDENT_OUTPUT);
     }
 
     public static String toJson(Object model){
-        String json = null;
-
+        log("toJson");
         try {
-             json = mapper.writeValueAsString(model);
+             String json = mapper.writeValueAsString(model);
              log(json);
+             return json;
 
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return json;
     }
 
-
-    public static void log(Object o){
-        System.out.println(o + "");
+    public static <T>T fromJson(String json, Class<T> clazz){
+        log("fromJson");
+        try {
+            T object = mapper.readValue(json, clazz);
+            log(object.toString());
+            return object;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static <T>void writeJsonToFile(String path, T model){
+        log("writeJsonToFile");
+        try {
+            mapper.writeValue(new File(path), model);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T>T readJsonFromFile(String path, Class<T> clazz){
+        log("readJsonFromFile");
+        try {
+            T model = mapper.readValue(new File(path), clazz);
+            log(model.toString());
+            return model;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
